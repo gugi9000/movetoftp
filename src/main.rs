fe6460e3e34419e7c2d57aca7af86bdfaa2bfa3a
Lucas Falch Sørensen <Lucas Falch Srensen>
2016-09-25 18:32:17 +0200

    Add option to delete emptied folders
    Closes #1

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ extern crate getopts;
 
 use std::env;
 use std::fs::{self, File};
+use std::path::Path;
 
 use ftp::FtpStream;
 use ftp::types::FileType;
@@ -35,8 +36,9 @@ fn main() {
     opts.optopt("s", "server", "set the hostname of the FTP-server (required)", "HOST");
     opts.optopt("p", "port", "set the port on the FTP-server (default: 21)", "PORT");
     opts.optopt("t", "to", "set the remote path on the FTP-server where the files will be moved to", "PATH");
-    opts.optopt("P", "password", "set the password of the user on the FTP-server to login with (required)", "PASSWORD");
     opts.optopt("u", "username", "set the username of the user on the FTP-server to login with (required)", "USERNAME");
+    opts.optopt("P", "password", "set the password of the user on the FTP-server to login with (required)", "PASSWORD");
+    opts.optflag("d", "delete", "deletes emptied folders after moving files");
     opts.optflag("h", "help", "prints this help");
 
     let matches = match opts.parse(&args){
@@ -49,12 +51,13 @@ fn main() {
         return
     }
 
-    let local_path = fs::read_dir(matches.opt_str("f").unwrap_or_else(|| ".".to_owned())).unwrap();
+    let local_path = matches.opt_str("f").unwrap_or_else(|| ".".to_owned());
     let hostname = required!(matches.opt_str("s"));
     let port_number: u16 = matches.opt_str("p").map(|p| p.parse().unwrap()).unwrap_or(21);
     let remote_path = matches.opt_str("t");
     let username = required!(matches.opt_str("u"));
     let password = required!(matches.opt_str("P"));
+    let delete_folders = matches.opt_present("d");
 
     println!("Connecting..");
 
@@ -65,18 +68,20 @@ fn main() {
         ftp_stream.cwd(p).unwrap();
     }
 
-    put_files(&mut ftp_stream, local_path, "./".to_owned());
+    put_files(&mut ftp_stream, local_path, "./".to_owned(), delete_folders);
 
     ftp_stream.quit().unwrap();
 
     println!("Finished!")
 }
 
-fn put_files(stream: &mut FtpStream, dir: fs::ReadDir, folder: String){
+fn put_files<P: AsRef<Path>>(stream: &mut FtpStream, dir: P, folder: String, delete: bool){
+    let dir = dir.as_ref();
+
     if folder != "./"{
         stream.mkdir(&folder).unwrap()
     }
-    for entry in dir{
+    for entry in fs::read_dir(dir).unwrap(){
         let entry = entry.unwrap();
 
         match entry.file_type().unwrap(){
@@ -96,9 +101,14 @@ fn put_files(stream: &mut FtpStream, dir: fs::ReadDir, folder: String){
                     }
                 }
             },
-            t if t.is_dir() => put_files(stream, fs::read_dir(entry.path()).unwrap(), entry.path().file_name().unwrap().to_string_lossy().into_owned()),
+            t if t.is_dir() => put_files(stream, entry.path(), entry.path().file_name().unwrap().to_string_lossy().into_owned(), delete),
             _ => ()
         }
     }
-
+    if delete{
+        match fs::remove_dir(dir){
+            Ok(()) => (),
+            Err(_) => println!("Couldn't remove folder {}", dir.display())
+        }
+    }
 }
